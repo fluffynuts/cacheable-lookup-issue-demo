5d3214642111ef8bb5f06882bc23654f517172fa
Davyd McColl <davydm@gmail.com>
2020-05-05 18:22:07 +0200

    :art: neater test.bat output

--- test.bat
+++ test.bat
@@ -1,8 +1,20 @@
-@echo test base endpoint (/user)
+@echo === test base endpoint (/user) ===
 curl http://localhost:10080/user
-@echo test end-point which calls back into /user via server.dev.local
+
+@echo.
+@echo.
+@echo === test end-point which calls back into /user via server.dev.local ===
 curl http://localhost:10080/fails
-@echo test by direct ip
+
+@echo.
+@echo.
+@echo === test by direct ip ===
 curl http://localhost:10080/by-ip
-@echo test via host server.dev.local with node inbuilt http
+
+@echo.
+@echo.
+@echo === test via host server.dev.local with node inbuilt http ===
 curl http://localhost:10080/by-http
+
+@echo.
+@echo.
